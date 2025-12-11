a7b5818f530e63af63c8f7ddc8538a867e05a0b1
emadnasri24 <emadnasri50@gmail.com>
2025-12-11 20:40:19 +0300

    make buttons active and cart view done

--- lib/features/cart/views/cart_view.dart
+++ lib/features/cart/views/cart_view.dart
@@ -1,39 +1,86 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hungry_app/features/cart/widgets/cart_item.dart';
 import 'package:hungry_app/shared/custom_button.dart';
 import 'package:hungry_app/shared/custom_text.dart';
 
-class CartView extends StatelessWidget {
+class CartView extends StatefulWidget {
   const CartView({super.key});
+
+  @override
+  State<CartView> createState() => _CartViewState();
+}
+
+class _CartViewState extends State<CartView> {
+  final int itemCount = 20;
+  late List<int> quantities;
+  @override
+  void initState() {
+    quantities = List.generate(itemCount, (_) => 1);
+    super.initState();
+  }
+
+  void onAdd(int index) {
+    setState(() {
+      quantities[index]++;
+    });
+  }
+
+  void onMin(int index) {
+    setState(() {
+      if (quantities[index] > 1) {
+        quantities[index]--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+        scrolledUnderElevation: 0, //لما بيعمل سكرول ما بيتغير اللون
+        backgroundColor: Colors.white,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: ListView.builder(
-          padding: EdgeInsets.only(bottom: 120, top: 100),
-          itemCount: 6,
+          padding: EdgeInsets.only(bottom: 120, top: 10),
+          itemCount: itemCount,
           itemBuilder: (context, index) {
             return CartItem(
               image: 'assets/detail/tomatow.png',
               text: 'Tomatow',
               desc: 'its color is red',
-              number: 1,
+              number: quantities[index],
+              onAdd: () => onAdd(index),
+              onMin: () => onMin(index),
             );
           },
         ),
       ),
       bottomSheet: Container(
-        color: Colors.white,
-        padding: EdgeInsets.all(10),
-        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 10,
+              offset: const Offset(0, -3),
+            ),
+          ],
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        height: 90,
         width: double.infinity,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CustomText(text: 'Total', size: 16),
