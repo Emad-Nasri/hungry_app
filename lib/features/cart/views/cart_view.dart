3911cdba70b1175e213ff6ff38099791fa9a2d85
emadnasri24 <emadnasri50@gmail.com>
2025-12-11 15:39:51 +0300

    product details view done..start cart view

--- lib/features/cart/views/cart_view.dart
+++ lib/features/cart/views/cart_view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Cart')));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          children: [
+            Gap(100),
+            Card(
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Image.asset('assets/detail/tomatow.png', width: 100),
+                    ],
+                  ),
+                  Column(children: [
+        
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
