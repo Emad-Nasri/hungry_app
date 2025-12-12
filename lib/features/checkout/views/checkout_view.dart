2f4bc65d8da13fa9b5360debd3b1c9124dd62787
emadnasri24 <emadnasri50@gmail.com>
2025-12-12 20:35:25 +0300

    added success dialog

--- lib/features/checkout/views/checkout_view.dart
+++ lib/features/checkout/views/checkout_view.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:hungry_app/core/constants/app_colors.dart';
 import 'package:hungry_app/features/checkout/widgets/order_details_widget.dart';
 import 'package:hungry_app/shared/custom_button.dart';
 import 'package:hungry_app/shared/custom_text.dart';
@@ -143,7 +145,74 @@ class _CheckoutViewState extends State<CheckoutView> {
                   CustomText(text: '\$18.9', size: 24),
                 ],
               ),
-              CustomButton(text: 'Pay now', onTap: () {}),
+              CustomButton(
+                text: 'Pay Now',
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        backgroundColor: Colors.transparent,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 200,
+                          ),
+                          child: Container(
+                            width: 300,
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.shade800,
+                                  blurRadius: 15,
+                                  offset: Offset(0, 0),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CircleAvatar(
+                                  radius: 40,
+                                  backgroundColor: AppColors.primary,
+                                  child: Icon(
+                                    CupertinoIcons.check_mark,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                ),
+                                Gap(10),
+                                CustomText(
+                                  text: 'Success!',
+                                  color: AppColors.primary,
+                                  size: 20,
+                                  weight: FontWeight.bold,
+                                ),
+                                Gap(3),
+                                CustomText(
+                                  text:
+                                      'Your payment was successful.\nA receipt for this purchase has\n been sent to your email.',
+                                  color: Colors.grey.shade400,
+                                  size: 11,
+                                ),
+                                Gap(10),
+                                CustomButton(
+                                  text: 'Close',
+                                  width: 200,
+                                  onTap: () => Navigator.pop(context),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ],
           ),
         ),
