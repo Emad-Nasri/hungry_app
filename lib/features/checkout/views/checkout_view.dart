bb2f7415f2eb73e1abbe15838f7bde96f67fafd0
emadnasri24 <emadnasri50@gmail.com>
2025-12-12 15:28:17 +0300

    created checkout widget had a row contained tow custom text and included it to checkout view

--- lib/features/checkout/views/checkout_view.dart
+++ lib/features/checkout/views/checkout_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:hungry_app/shared/custom_text.dart';
 
 class CheckoutView extends StatelessWidget {
@@ -17,15 +18,43 @@ class CheckoutView extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CustomText(
               text: 'Order summary',
               size: 20,
               weight: FontWeight.w500,
             ),
+            Gap(10),
+            checkoutWidget('Order', '18.5'),
+            Gap(10),
+            checkoutWidget('Taxes', '3.5'),
+            Gap(10),
+            checkoutWidget('Delivery fees', '2.4'),
+            Gap(10),
           ],
         ),
       ),
     );
   }
+}
+
+Widget checkoutWidget(title, price) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      CustomText(
+        text: title,
+        size: 15,
+        weight: FontWeight.w400,
+        color: Colors.grey.shade600,
+      ),
+      CustomText(
+        text: '$price\$',
+        size: 15,
+        weight: FontWeight.w400,
+        color: Colors.grey.shade600,
+      ),
+    ],
+  );
 }
