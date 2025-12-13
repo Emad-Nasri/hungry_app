f9c8f7c9912d21f5cb586211748d9665598a3556
emadnasri24 <emadnasri50@gmail.com>
2025-12-13 13:27:25 +0300

    added animation to splash view

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry_app/core/constants/app_colors.dart';
+import 'package:hungry_app/root.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,21 +11,79 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView> {
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _logoFade;
+  late Animation<Offset> _imageSlide;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    /// Fade فقط للّوجو (بدون تصغير)
+    _logoFade = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    /// Slide للصورة السفلية
+    _imageSlide = Tween<Offset>(
+      begin: const Offset(0, 0.5),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => Root()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Gap(280), //=SizedBox(height: 280),
-            SvgPicture.asset('assets/logo/logo.svg'),
-            Spacer(),
-            Image.asset('assets/splash/splash.png'),
-          ],
-        ),
+      body: Column(
+        children: [
+          const Gap(280),
+
+          /// LOGO (حجم ثابت)
+          FadeTransition(
+            opacity: _logoFade,
+            child: SvgPicture.asset(
+              'assets/logo/logo.svg',
+              width: 140,
+              height: 50,
+            ),
+          ),
+
+          const Spacer(),
+
+          /// Bottom Image Animation
+          SlideTransition(
+            position: _imageSlide,
+            child: Image.asset(
+              'assets/splash/splash.png',
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
       ),
     );
   }
