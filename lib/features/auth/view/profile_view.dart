386a523b2c7b9e62e59e55de5822aa96dd7fd11b
emadnasri24 <emadnasri50@gmail.com>
2025-12-14 19:05:37 +0300

    compare profile frome state less to state ful

--- lib/features/auth/view/profile_view.dart
+++ lib/features/auth/view/profile_view.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry_app/core/constants/app_colors.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
 
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
     TextEditingController _name = TextEditingController();
