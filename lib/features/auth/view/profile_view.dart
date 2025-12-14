a8d20c6fc19d952507c4dea0b7261072240cf112
emadnasri24 <emadnasri50@gmail.com>
2025-12-14 18:38:05 +0300

    chnge background color to green in profile view

--- lib/features/auth/view/profile_view.dart
+++ lib/features/auth/view/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hungry_app/core/constants/app_colors.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -6,8 +7,9 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primary,
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: AppColors.primary,
         leading: GestureDetector(
           onTap: () => Navigator.pop(context),
           child: Icon(Icons.arrow_back),
