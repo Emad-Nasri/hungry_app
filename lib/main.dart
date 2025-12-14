e22a5a8a111a6e16911d51eddc5485d6b67632cc
emadnasri24 <emadnasri50@gmail.com>
2025-12-14 21:27:52 +0300

    معالجة مشكلة عرض الشاشة بشكل افقي

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hungry_app/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
