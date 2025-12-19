2663fec24bd4338afd0ced82ebec154b8ed0d7be
emadnasri24 <emadnasri50@gmail.com>
2025-12-19 23:28:54 +0300

    definded many variables and added constructor in user model

--- lib/features/auth/data/user_model.dart
+++ lib/features/auth/data/user_model.dart
@@ -1,1 +1,17 @@
-class UserModel {}
+class UserModel {
+  final String name;
+  final String email;
+  final String? image;
+  final String? token;
+  final String? visa;
+  final String? address;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.image,
+    required this.token,
+    required this.address,
+    required this.visa,
+  });
+}
