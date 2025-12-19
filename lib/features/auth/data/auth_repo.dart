d099f136190d02436fdacedd1eccce1228cbd0f0
emadnasri24 <emadnasri50@gmail.com>
2025-12-20 00:02:03 +0300

    added login method in auth repo

--- lib/features/auth/data/auth_repo.dart
+++ lib/features/auth/data/auth_repo.dart
@@ -1,1 +1,36 @@
-class AuthRepo {}
+import 'package:dio/dio.dart';
+import 'package:hungry_app/core/network/api_error.dart';
+import 'package:hungry_app/core/network/api_exceptions.dart';
+import 'package:hungry_app/core/network/api_service.dart';
+import 'package:hungry_app/core/utils/pref_helper.dart';
+import 'package:hungry_app/features/auth/data/user_model.dart';
+
+class AuthRepo {
+  ApiService apiService = ApiService();
+
+  //LOGIN
+  Future<UserModel> login(String email, String password) async {
+    try {
+      final response = await apiService.post('/login', {
+        'email': email,
+        'password': password,
+      });
+      final user = UserModel.fromJson(response['data']);
+      if (user.token != null) {
+        await PrefHelper.saveToken(user.token!);
+      }
+      return user;
+    } on DioError catch (e) {
+      throw ApiExceptions.handleError(e);
+    } catch (e) {
+      throw ApiError(message: e.toString());
+    }
+  }
+  //REGISTER
+
+  //GET PROFILE DATA
+
+  //UPDATE PROFILE DATA
+
+  //LOGOUT
+}
