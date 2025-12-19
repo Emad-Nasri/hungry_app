78f8cd91109651dc1a80115c3b275e58f2a9be26
emadnasri24 <emadnasri50@gmail.com>
2025-12-19 22:55:39 +0300

    added get method

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -1,1 +1,23 @@
-class ApiService {}
+import 'package:dio/dio.dart';
+import 'package:hungry_app/core/network/api_exceptions.dart';
+import 'package:hungry_app/core/network/dio_client.dart';
+
+class ApiService {
+  final DioClient _dioClient = DioClient();
+  //CRUD METHODS
+
+  //GET
+  Future<dynamic> get(String endPoint) async {
+    try {
+      final response = await _dioClient.dio.get(endPoint);
+      return response.data;
+    } on DioError catch (e) {
+      ApiExceptions.handleError(e);
+    }
+  }
+  //POST
+
+  //PUT OR UPDATE
+
+  //DELETE
+}
