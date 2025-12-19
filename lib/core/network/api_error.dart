8dac014d43e34f72a582f491a4c744776fb9d9ca
emadnasri24 <emadnasri50@gmail.com>
2025-12-19 22:41:50 +0300

    added handleError method

--- lib/core/network/api_error.dart
+++ lib/core/network/api_error.dart
@@ -2,7 +2,7 @@ class ApiError {
   final String message;
   final int? statusCode;
 
-  ApiError({required this.message, required this.statusCode});
+  ApiError({required this.message, this.statusCode});
   @override
   String toString() {
     return 'Error is : $message (Status code is $statusCode)';
--- lib/core/network/api_exceptions.dart
+++ lib/core/network/api_exceptions.dart
@@ -1,1 +1,15 @@
-class ApiExceptions {}
+import 'package:dio/dio.dart';
+import 'package:hungry_app/core/network/api_error.dart';
+
+class ApiExceptions {
+  static ApiError handleError(DioError error) {
+    switch (error.type) {
+      case DioErrorType.connectionTimeout:
+        return ApiError(message: 'Bad Connection');
+      case DioErrorType.badResponse:
+        return ApiError(message: error.toString());
+      default:
+        return ApiError(message: 'Something Went Wrong');
+    }
+  }
+}
